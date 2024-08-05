cebcf334d671051446e294e38f89fad90bfb8238
ozmberkan <ozmberkan@gmail.com>
2024-08-05 20:11:24 +0300

    fixed bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <div className="w-full h-[500px] bg-blue-200 -z-10 flex justify-center items-center flex-col relative">
-        <img src={Banner} alt="" className="w-full h-[500px] absolute" />
+        <img src={Banner} alt="" className="w-full absolute" />
         <div className="z-20">
           <h1 className="text-[120px] font-Nunito tracking-tighter text-white font-bold">
             HIGHYFLY
